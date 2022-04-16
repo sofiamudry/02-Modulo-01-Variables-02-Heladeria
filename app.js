139e2f6995fc9cac787d1f0109107c1420cfe16d
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-16 11:08:23 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('Heladeria');
+
+alert('Hola, bienvenide a la Heladeria ...');
+
+const gusto01 = prompt('Ingrese el primer gusto');
+const gusto02 = prompt('Ingrese el segundo gusto');
+const gusto03 = prompt('Ingrese el tercer gusto');
+
+alert(`Aqui tiene su helado de ${gusto01}${','} ${gusto02} ${'y'} ${gusto03}`);
